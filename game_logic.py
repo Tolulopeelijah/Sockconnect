ec84e546b23adf037349684bb01de8bbbb88c11f
Tolulope <tolulopee90@gmail.com>
2025-07-01 18:07:06 +0100

    implement tictactoe logic

--- game_logic.py
+++ game_logic.py
@@ -9,5 +9,33 @@ def display(board):
             print('\n' + '-'*13, end = '\n| ')
     print('\n' + '-'*13)
     
+def validate(board, move):
+    return board[move] == '#'
 
-display(board)
+def check_win(board):
+    winning_positions = ['012', '345', '678', '036', '147', '258', '048', '246']
+    if '#' not in board:
+        return 'draw'
+    for pos in winning_positions:
+        X_wins = []
+        Y_wins = []
+        for i in pos:
+            X_wins.append(board[int(i)] == 'X')
+            Y_wins.append(board[int(i)] == 'X')
+        if all(X_wins): return 'X'
+        if all(Y_wins): return 'Y'
+
+
+
+
+def update(board, move, player): #player enter 1 ahead of index
+    move = int(move)
+    if not validate(board, move-1):
+        return f'The space has been taken by player {player}'
+    board[move-1] = player
+    if check_win(board):
+        return check_win(board) 
+        print(f'{check_win} won')
+
+
+    display(board)
--- server.py
+++ server.py
@@ -1,32 +1,57 @@
-messages = ['']
 import socket
 import threading
+import game_logic as game
 
-# Function to handle client connection
-def handle_client(client_socket, address):
-    print(f"[NEW CONNECTION] {address} connected.")
+logging = True
+board = game.board
+client_m = ['X',
+            'O',
+            'Resign',
+            'Draw',
+            ] # client's possible messages
+server_m = ['Connection established',
+            'Player 1/2 plays X/O',
+            'Player 1/2 Resigned',
+            'Invalid move',
+            'Player 1/2 won',
+            "It's a tie"]
+
+
+
+
+def handle_client(client_socket, address, player = {}):
+    player[address] = 'X' if player else 'O'
+    print(f"Player {len(player)} connected")
+    if logging and len(player) == 2:
+        game.display(board)
     while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            if not message:
-                break
-            print(f"[{address}] {message}")
-            client_socket.send("Message received".encode())
-        except:
+        move = client_socket.recv(1024).decode()
+        if game.validate(board, move):
+            game.update(board, move, player[address])
             break
+        else:
+            client_socket.send("The move has been occupied".encode())
 
-    print(f"[DISCONNECTED] {address} disconnected.")
+
+    print(f"Player {player[address]} disconnected")
+
     client_socket.close()
 
-# Main server setup
+
+
+
+
+
+
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 12345))
-server.listen(5)
+server.bind(('localhost', 8000))
+server.listen(1)
 
-print("[STARTED] Server is listening on port 12345")
+if logging: print("Waiting for users to connect")
 
-while True:
+for i in range(2):
     client_socket, addr = server.accept()
     thread = threading.Thread(target=handle_client, args=(client_socket, addr))
     thread.start()
-    print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
