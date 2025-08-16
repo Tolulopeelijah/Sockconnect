58573ff3403c60e21349564d813aee4fa9016ae3
Tolulope <tolulopee90@gmail.com>
2025-08-16 02:34:41 +0100

    added more flow

--- client.py
+++ client.py
@@ -2,13 +2,20 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 8000))
+print('Welcome to the TICTACTOE Game')
 
-while True:
-    message = input("You: ")
-    if message.lower() == "exit":
-        break
-    client.send(message.encode())
+for i in range(9):
     response = client.recv(1024).decode()
-    print("Server:", response)
+    if response == "your turn": 
+        while True:
+            message = input("Please make your move(exit to cancel): ")
+            if message.lower() == "exit":
+                break
+            client.send(message.encode())
+            if client.recv(1024).decode() == 'valid move':
+                break
+            else: print('Invalid move!')
+
+
 
 client.close()
