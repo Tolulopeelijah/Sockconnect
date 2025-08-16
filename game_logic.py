87897c7746cb37fbc4b8d3377908525c4579b28b
Tolulope <tolulopee90@gmail.com>
2025-08-16 02:35:11 +0100

    created interface

--- game_logic.py
+++ game_logic.py
@@ -1,4 +1,5 @@
 board = ['#']* 9
+game_name = 'Tic-Tac-Toe'
 
 def display(board):
     print('\n' + '-'*13, end = '\n| ')
@@ -9,9 +10,13 @@ def display(board):
     print('\n' + '-'*13)
     
 def validate(board, move)->bool:
+    try:
+        move = int(move)
+    except:
+        return False
     return board[move] == '#'
 
-def check_win(board)->['draw', 'X', 'Y']:
+def check_win(board)->['draw', 'X', 'O']:
     winning_positions = ['012', '345', '678', '036', '147', '258', '048', '246']
     if '#' not in board:
         return 'draw'
@@ -20,15 +25,18 @@ def check_win(board)->['draw', 'X', 'Y']:
         Y_wins = []
         for i in pos:
             X_wins.append(board[int(i)] == 'X')
-            Y_wins.append(board[int(i)] == 'Y')
+            Y_wins.append(board[int(i)] == 'O')
         if all(X_wins): return 'X'
-        if all(Y_wins): return 'Y'
+        if all(Y_wins): return 'O'
 
-def update(board, move, player)->['draw', 'X', 'Y', 'space taken']: #player enter 1 ahead of index
-    move = int(move)
-    if not validate(board, move-1):
+def update(board, move, player)->['draw', 'X', 'O', 'space taken']: #player enter 1 ahead of index
+
+    move = int(move) - 1
+    if validate(board, move):
+        board[move-1] = player
+    else:
         return 'taken', f'The space has been taken by player {player}'
-    board[move-1] = player
+    
     if check_win(board):
         return check_win(board) 
         print(f'{check_win} won')
@@ -36,12 +44,16 @@ def update(board, move, player)->['draw', 'X', 'Y', 'space taken']: #player ente
 
     display(board)
 
+def interface(player1, board, label):
+    if validate:
+        update(board, player1, label)
+        
 
 if __name__ == "__main__":
     print('Welcome to the tictactoe game!')
     display(board)
     while not check_win(board):
-        for i in ['X', 'Y']:
+        for i in ['X', 'O']:
             while (x:= update(board, input(f"{i}'s turn: "), i)):
                 if x[0] == 'taken': print('The space has been taken')
                 elif x == 'draw': 
