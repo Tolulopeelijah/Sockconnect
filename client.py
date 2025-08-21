f6da20d2c27bfccdf07c00a78579d51724ff990e
Tolulope <tolulopee90@gmail.com>
2025-08-22 00:19:51 +0100

    reimplementing the protocol

--- client.py
+++ client.py
@@ -4,17 +4,32 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 8000))
 print('Welcome to the TICTACTOE Game')
 
+message = None
 for i in range(9):
-    response = client.recv(1024).decode()
+    if message in ['won', 'lost', 'draw']:
+        break
+    response = client.recv(1024).decode() # signal to know it's your turn
     if response == "your turn": 
         while True:
-            message = input("Please make your move(exit to cancel): ")
-            if message.lower() == "exit":
+            move = input("Please make your move(exit to cancel): ")
+            if move.lower() == "exit":
                 break
-            client.send(message.encode())
-            if client.recv(1024).decode() == 'valid move':
+            client.send(move.encode())
+            message = client.recv(1024).decode() # valid or invalid move feedback
+            if message == 'valid move':
                 break
-            else: print('Invalid move!')
+            elif message == 'won':
+                print('yipee, you won')
+                break
+            elif message == 'lost':
+                print("sorry, this didn't come your way")
+                break
+            elif message == 'draw':
+                print('the game is a tie!')
+                break
+            else: 
+                print(message)
+                print('Invalid move!')
 
 
 
